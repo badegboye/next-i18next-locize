67237375302f2196bf6bc6f45fc5d63b36a1be7c
Bade Adegboye <badeade@hotmail.com>
2021-11-29 12:24:37 +0000

    lokalise test

---
+++ next-i18next.config.js
@@ -0,0 +1,12 @@
+const path = require('path');
+module.exports = {
+  i18n: {
+    defaultLocale: 'en',
+    locales: ['en', 'de'],
+    localePath: path.resolve('./public/locales'),
+  },
+  react: {
+    useSuspense: false,
+    wait: true,
+  },
+};
